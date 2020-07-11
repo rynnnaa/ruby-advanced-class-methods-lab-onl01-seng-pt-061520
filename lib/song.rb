be11d0b4df5d243f10f82fbf871878b76d49e703
Ryna Truong <rynatruong@outlook.com>
2020-07-11 15:36:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -9,5 +9,30 @@ class Song
   def save
     self.class.all << self
   end
-
-end
+  
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(str)
+    song = Song.new
+    song.name = str
+    song
+  end
+  
+  def self.create_by_name(str)
+    song = self.create
+    song.name = str
+    song
+  end
+  
+  def self.find_by_name(str)
+    self.all.find {|string|string.name == str}
+    end
+  end
+  
+  def find_or_create_by_name(str)
+    self.find_by_name(str) || self.create_by_name(str)
+  end
