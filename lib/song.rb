e34aacda68c72aa17e2bf156999b84157ec8f0ae
Ryna Truong <rynatruong@outlook.com>
2020-07-11 16:19:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -57,10 +57,10 @@ class Song
     artist_name = parts[0]
     song_name= parts[1].gsub(".mp3", "") 
     
-    song = self.create
-    song.artist_name = artist_name
-    song.name = song_name
-    song
+    # song = self.create
+    # song.artist_name = artist_name
+    # song.name = song_name
+    # song
   end
   
   def self.destroy_all
